6594fa72f4032270fc805ba26ec87458a27d6309
Nikodem <nikodem.zychma@gmail.com>
2023-06-19 21:59:36 +0200

    fix: clean up

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { baseURL } from "./constants/config";
 import { Morizon } from "./services/morizon";
 import { Otodom } from "./services/otodom";
 
-const Scraper = new Otodom({city: 'wroclaw', type: 'rent', areaLow: 50, areaHigh: 80, priceLow: 3900, priceHigh: 4300});
-const Scraper2 = new Morizon({city: 'wroclaw', type: 'rent', propertyType: 'mieszkania', areaLow: 50, areaHigh: 400, priceHigh: 3500, priceLow: 3300});
-// Scraper.initScrape();
-Scraper.rescrapePropertiesFromExcel();
+const ScraperOtodom1 = new Otodom({city: 'wroclaw', type: 'rent', areaLow: 50, areaHigh: 80, priceLow: 3900, priceHigh: 4300});
+// const ScraperOtodom2 = new Otodom({city: 'wroclaw', type: 'rent'});
+// const Scraper2 = new Morizon({city: 'wroclaw', type: 'rent', propertyType: 'mieszkania', areaLow: 50, areaHigh: 400, priceHigh: 3500, priceLow: 3300});
+
+
+
+ScraperOtodom1.initScrape();
+// ScraperOtodom1.rescrapePropertiesFromExcel();
