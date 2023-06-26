9db94566a5fc884870ee4c5c3c2cbf1c3b93657a
Nikodem <nikodem.zychma@gmail.com>
2023-06-26 18:30:55 +0100

    fix: ??? strange issue, #todo

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,7 @@ import { Morizon } from "./services/morizon";
 import { Otodom } from "./services/otodom";
 import { Server } from './server';
 
-try {
-    const server = new Server();
-    server.start(3000);
-} catch(error) {
-    console.error(error)
-}
+const helperClass = new Otodom({city: 'xyz', type: 'rent', ownerTypeSearching: 'PRIVATE', areaLow: 0, areaHigh: 0, priceLow: 0, priceHigh: 0});
+
+const server = new Server();
+server.start(3000);
